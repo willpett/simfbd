d008711d6575d6a9bf29eb23d78ed9ba41a20160
Walker Pett <will.pett@gmail.com>
2018-08-08 00:26:21 -0500

    adding mixed specation sim

--- bin/sim.py
+++ bin/sim.py
@@ -4,6 +4,7 @@ import multiprocessing as mp
 import subprocess
 import sys
 import os
+import re
 
 from optparse import OptionParser
 
@@ -12,29 +13,33 @@ parser.add_option("-n", "--nreps", dest="n",
                   help="number of replicates", metavar="N")
 parser.add_option("-k", "--ncpu", dest="k",
                   help="number of cpus", metavar="K")
-parser.add_option("-m", "--model", dest="model",
-                  help="morphological model to use", default="none", metavar="MODEL")
-parser.add_option("-t", "--tree",
-                  action="store_true", dest="tree", default=False,
-                  help="use the full FBD tree model")
+parser.add_option("-s", "--sim", dest="sim",
+                  help="character simulation model", default="none", metavar="SIM")
+parser.add_option("-i", "--infer", dest="infer",
+                  help="inference model", default="range", metavar="SIM")
 parser.add_option("-f", "--fixed",
                   action="store_true", dest="fixed", default=False,
-                  help="use fixed FBD tree")
-
+                  help="use fixed tree simulation")
+parser.add_option("-x", "--xdiag",
+                  action="store_true", dest="fixed", default=False,
+                  help="don't diagnose species")
 
 (options, args) = parser.parse_args()
 
-models = ['asym','mk','none']
+char_models = ['none','asym','mk']
+inf_models = ['range','tree','mk','mixed','mk-mixed']
 
-if options.model not in models:
+if options.sim not in char_models:
 	raise Exception("Morphological model must be one of: "+str(models))
 
-if options.model == "mk" and not os.path.isfile("rateprior.rev"):
+if options.sim == "mk" and not os.path.isfile("rateprior.rev"):
 	raise Exception("rateprior.rev file not found")
 
 if (not os.path.isfile("params.r") or not os.path.isfile("prior.rev")) and not options.fixed:
 	raise Exception("params.r/prior.rev file not found")
 
+if re.search('mk',options.infer) and options.sim == 'none':
+	raise Exception("cannot infer mk without simulating character data")
 
 os.system('rm -rf sims')
 os.system('mkdir sims')
@@ -58,26 +63,31 @@ def simulate(i):
 		os.system('cp ../../taxa.sa.tsv ./')
 		os.system('cp ../../tree.sa.tre ./')		
 
-	if options.model == 'asym':
+	if options.sim == 'asym':
 		os.system('rb '+bindir+'/sim-asym.rev')
 		os.system('cp taxa.fbd.tsv diagnosed.taxa')
 		os.system('cp asym.nex diagnosed.nex')
-	elif options.model == 'mk' or options.fixed:
+	elif options.sim == 'mk':
 		os.system('rb '+bindir+'/sim-mk.rev')
 		os.system(bindir+'/diagnose.py mk.fa taxa.sa.tsv')
 	else:
 		os.system('cp taxa.fbd.tsv diagnosed.taxa')
 
-	if options.tree:
-		if options.model == 'none':
-			os.system('rb '+bindir+'/infer-tree.rev')
-			os.system(bindir+'/summarize.r')
-		else:
-			os.system('rb '+bindir+'/infer-mk.rev')
-			os.system(bindir+'/summarize.r 1')
+	if options.infer == 'mk-mixed':
+		os.system('rb '+bindir+'/infer-mk-mixed.rev')
+	elif options.infer == 'mk':
+		os.system('rb '+bindir+'/infer-mk.rev')
+	elif options.infer == 'mixed':
+		os.system('rb '+bindir+'/infer-tree-mixed.rev')
+	elif options.infer == 'tree':
+		os.system('rb '+bindir+'/infer-tree.rev')
 	else:
 		os.system('rb '+bindir+'/infer-range.rev')
-		os.system(bindir+'/summarize.r')
+
+	summ = bindir+'/summarize.r'
+	if re.search('mk',options.infer):
+		summ += ' 1'
+	os.system(summ)
 
 pool = mp.Pool(processes=int(options.k))
 
